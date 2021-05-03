d93a3a39e3a8ad2660af66fb3a6eb99a65851e27
Goga Eva <igorevtushenko22@gmail.com>
2021-05-03 03:48:15 +0300

    implement tested-binary module

--- build/cmd/bood/main.go
+++ build/cmd/bood/main.go
@@ -7,11 +7,9 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/gogaeva/build-system/build/gomodule"
 	"github.com/google/blueprint"
 	"github.com/roman-mazur/bood"
-	"github.com/roman-mazur/bood/gomodule"
-	// TODO: Підставте свій власний пакет.
-	// "github.com/roman-mazur/design-practice-1-template/build/gomodule"
 )
 
 var (
@@ -21,8 +19,7 @@ var (
 
 func NewContext() *blueprint.Context {
 	ctx := bood.PrepareContext()
-	// TODO: Замініть імплементацію go_binary на власну.
-	ctx.RegisterModuleType("go_binary", gomodule.SimpleBinFactory)
+	ctx.RegisterModuleType("go_binary", gomodule.TestedBinFactory)
 	return ctx
 }
 
--- build/gomodule/tested-binary.go
+++ build/gomodule/tested-binary.go
@@ -1,15 +1,125 @@
 package gomodule
 
-import "github.com/google/blueprint"
+import (
+	"fmt"
+	"path"
+
+	"github.com/google/blueprint"
+	"github.com/roman-mazur/bood"
+)
+
+var (
+	pctx = blueprint.NewPackageContext("github.com/goga-eva/build-system/build/gomodule")
+
+	goBuild = pctx.StaticRule("binaryBuild", blueprint.RuleParams{
+		Command:     "cd $workDir && go build -gcflags=\"all=-N -l\" -o $outputPath $pkg",
+		Description: "build go command $pkg",
+	}, "workDir", "outputPath", "pkg")
+
+	goVendor = pctx.StaticRule("vendor", blueprint.RuleParams{
+		Command:     "cd $workDir && go mod vendor",
+		Description: "vendor dependencies of $name",
+	}, "workDir", "name")
+
+	goTest = pctx.StaticRule("test", blueprint.RuleParams{
+		Command:     "cd $workDir && go test -v $pkg > $outputPath",
+		Description: "test $pkg",
+	}, "workDir", "pkg", "outputPath")
+)
 
 type testedBinaryModule struct {
 	blueprint.SimpleName
 
 	properties struct {
-		// TODO: Визначте поля структури, щоб отримати дані з визначень у файлі build.bood
+		Pkg         string
+		Srcs        []string
+		SrcsExclude []string
+		TestPkg     string
+		TestSrcs    []string
+		VendorFirst bool
+		Deps        []string
 	}
 }
 
+func (tb *testedBinaryModule) DynamicDependencies(blueprint.DynamicDependerModuleContext) []string {
+	return tb.properties.Deps
+}
+
 func (tb *testedBinaryModule) GenerateBuildActions(ctx blueprint.ModuleContext) {
-	// TODO: Імплементууйте генерацію правил збірки для ninja-файла.
+	name := ctx.ModuleName()
+	config := bood.ExtractConfig(ctx)
+	config.Debug.Printf("Adding build actions for bo binary module %s", name)
+
+	outputPath := path.Join(config.BaseOutputDir, "bin", name)
+	outputTestsPath := path.Join(config.BaseOutputDir, "reports", name, "test.txt")
+
+	var inputs []string
+	var testInputs []string
+	inputErrors := false
+	for _, src := range tb.properties.Srcs {
+		if matches, err := ctx.GlobWithDeps(src, tb.properties.SrcsExclude); err == nil {
+			inputs = append(inputs, matches...)
+		} else {
+			ctx.PropertyErrorf("srcs", "Cannot resolve files that match pattern %s", src)
+			inputErrors = true
+		}
+	}
+	for _, testSrc := range tb.properties.TestSrcs {
+		if matches, err := ctx.GlobWithDeps(testSrc, []string{}); err == nil {
+			testInputs = append(testInputs, matches...)
+		} else {
+			ctx.PropertyErrorf("srcs", "Cannot resolve files that match pattern %s", testSrc)
+			inputErrors = true
+		}
+	}
+	if inputErrors {
+		return
+	}
+
+	if tb.properties.VendorFirst {
+		vendorDirPath := path.Join(ctx.ModuleDir(), "vendor")
+		ctx.Build(pctx, blueprint.BuildParams{
+			Description: fmt.Sprintf("Vendor dependencies of %s", name),
+			Rule:        goVendor,
+			Outputs:     []string{vendorDirPath},
+			Implicits:   []string{path.Join(ctx.ModuleDir(), "go.mod")},
+			Optional:    true,
+			Args: map[string]string{
+				"workDir": ctx.ModuleDir(),
+				"name":    name,
+			},
+		})
+		inputs = append(inputs, vendorDirPath)
+	}
+
+	ctx.Build(pctx, blueprint.BuildParams{
+		Description: fmt.Sprintf("Build %s as Go binary", name),
+		Rule:        goBuild,
+		Outputs:     []string{outputPath},
+		Implicits:   inputs,
+		Args: map[string]string{
+			"workDir":    ctx.ModuleDir(),
+			"pkg":        tb.properties.Pkg,
+			"outputPath": outputPath,
+		},
+	})
+
+	if tb.properties.TestPkg != "" {
+		ctx.Build(pctx, blueprint.BuildParams{
+			Description: fmt.Sprintf("Test %s", name),
+			Rule:        goTest,
+			Outputs:     []string{outputTestsPath},
+			Implicits:   testInputs,
+			Args: map[string]string{
+				"workDir":    ctx.ModuleDir(),
+				"pkg":        tb.properties.TestPkg,
+				"outputPath": outputTestsPath,
+			},
+		})
+	}
+}
+
+func TestedBinFactory() (blueprint.Module, []interface{}) {
+	tbm := &testedBinaryModule{}
+	return tbm, []interface{}{&tbm.SimpleName.Properties, &tbm.properties}
 }
